d4b710101ca8a25c6d6d0f0d0d4e6e3814afd0aa
lbaksa <luka.baksa@emilfreydigital.hr>
2024-11-09 17:32:13 +0100

    fix: fix user db model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,33 @@
 import { Model, DataTypes } from "sequelize";
 import sequelize from "../config/database";
 import bcrypt from "bcrypt";
-class User extends Model {
+
+export enum MusicGenre {
+  ROCK = "Rock",
+  JAZZ = "Jazz",
+  CLASSICAL = "Classical",
+  POP = "Pop",
+  HIPHOP = "Hip-Hop",
+  ELECTRONIC = "Electronic",
+  BLUES = "Blues",
+  COUNTRY = "Country",
+  REGGAE = "Reggae",
+  METAL = "Metal",
+}
+
+export enum Instrument {
+  GUITAR = "Guitar",
+  PIANO = "Piano",
+  DRUMS = "Drums",
+  VIOLIN = "Violin",
+  BASS = "Bass",
+  SAXOPHONE = "Saxophone",
+  FLUTE = "Flute",
+  CELLO = "Cello",
+  TRUMPET = "Trumpet",
+  VOCALS = "Vocals",
+}
+export class User extends Model {
   public id!: number;
   public username!: string;
   public email!: string;
@@ -9,46 +35,58 @@ class User extends Model {
   public createdAt!: Date;
   public updatedAt!: Date;
 }
-User.beforeCreate(async(user) => {
+User.beforeCreate(async (user) => {
   user.password = await bcrypt.hash(user.password, 15);
-})
+});
+
 User.init(
   {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
-      primaryKey: true
+      primaryKey: true,
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true
-      }
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mainInstrument: {
+      type: DataTypes.ENUM(...Object.values(Instrument)),
+      allowNull: false,
+    },
+    genresOfInterest: {
+      type: DataTypes.ARRAY(DataTypes.ENUM(...Object.values(MusicGenre))),
+      allowNull: false,
     },
     createdAt: {
       type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
+      defaultValue: DataTypes.NOW,
     },
     updatedAt: {
       type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
-    }
+      defaultValue: DataTypes.NOW,
+    },
   },
   {
     sequelize,
     tableName: "users",
-    timestamps: true
+    timestamps: true,
   }
 );
 
-export default User;
