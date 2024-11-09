73af4a11cb7d8beb065564464a105a03e9b41afb
lbaksa <luka.baksa@emilfreydigital.hr>
2024-11-09 16:56:15 +0100

    fix: remove unsued vars

--- src/controllers/auth.controller.ts
+++
@@ -1,7 +0,0 @@
-import {Request, Response } from "express";
-import * as userSerivce from "@/services/user.service"
-import { LoginRequest } from "@types/auth";
-
-export const loginUser = async (loginRequest: LoginRequest) => {
-
-}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,6 @@
 import express from "express";
-import * as authController from "@controllers/auth.controller";
+
 
 const router = express.Router();
-
-router.post("/login", authController.loginUser);
 
 export default router;
