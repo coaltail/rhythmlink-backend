64d474b7af96d60674f43a15f156bf125e5dd3f4
tcosic22 <tcosic22@student.foi.hr>
2025-01-29 23:59:54 +0100

    feat: Implement group request
    Added groupRequest.
    Updated group.controller, groupMembers, group.routes, group.service and group interface.

--- src/controllers/group.controller.ts
+++ src/controllers/group.controller.ts
@@ -1,124 +1,280 @@
-import { GroupCreateRequest, GroupCreateResponse, GroupGetResponse } from "@interface/group";
+import {
+  GroupCreateRequest,
+  GroupCreateResponse,
+  GroupGetByUserResponse,
+  GroupGetRequestsResponse,
+  GroupGetResponse
+} from "@interface/group";
 import { Request, Response } from "express";
 import logger from "@utils/logger";
 import { validationResult } from "express-validator";
-import * as userService from "@services/user.service"
-import { ApiError, ApiValidationError, UserNotFoundError } from "@common/errors";
+import * as userService from "@services/user.service";
+import {
+  ApiError,
+  ApiValidationError,
+  UserNotFoundError
+} from "@common/errors";
 import { MusicGenre } from "@models/user";
-import * as groupService from "@services/group.service"
+import * as groupService from "@services/group.service";
 import { IRequestUser } from "@interface/auth";
 import { HttpStatusCode } from "@common/httpStatusCodes";
 
-export const createNewGroup = async (req: IRequestUser, res: Response<GroupCreateResponse | { message: string }>) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            throw new ApiValidationError("Validation failed", errors.array());
-        }
-        const { groupData } = req.body
-        const mainImage = req.file
-        const data: GroupCreateRequest = JSON.parse(groupData);
-        const owner = userService.findUserById(data.ownerId);
-        if (!owner) {
-            throw new UserNotFoundError(`Owner with id ${data.ownerId} not found.`);
-        }
-        const allowedImageTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-        if (!allowedImageTypes.includes(mainImage.mimetype)) {
-            throw new ApiValidationError("Invalid image type. Only JPG, PNG, and JPEG are allowed.", []);
-        }
-
-        const userId = req.user.userId;
-        if (userId != data.ownerId) {
-            throw new ApiValidationError("Cannot use another owner as a group owner.", []);
-        }
-
-        const genreArray: Array<MusicGenre> = data.genres;
-        const newGroup = await groupService.createNewGroup(data.ownerId, data.name, genreArray, mainImage)
-        res.status(201).json(newGroup);
-    } catch (error: unknown) {
-
-        logger.error("Create new group error: ", error);
-        if (error instanceof ApiError) {
-            res.status(error.statusCode).json({
-                message: error.message,
-                ...(error instanceof ApiValidationError && { errors: error.errors })
-            })
-            return;
-        }
-        res.status(500).json({ message: "Internal server error" });
+export const createNewGroup = async (
+  req: IRequestUser,
+  res: Response<GroupCreateResponse | { message: string }>
+) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new ApiValidationError("Validation failed", errors.array());
     }
-}
-
-export const getGroup = async (req: IRequestUser, res: Response<GroupGetResponse | { message: string }>) => {
-    try {
-
-        const id = parseInt(req.params.id, 10);
-
-        if (isNaN(id)) {
-          res.status(HttpStatusCode.BAD_REQUEST).json({ message: "Invalid group ID." });
-          return;
-        }
-  
-      const getGroupResponse = await groupService.getGroup(id);
-  
-      res.status(HttpStatusCode.OK).json(getGroupResponse);
-
-    } catch (error: unknown) {
-      logger.error("Getting group error: ", error);
-      if (error instanceof ApiError) {
-        res.status(error.statusCode).json({
-          message: error.message,
-          ...(error instanceof ApiValidationError && { errors: error.errors })
-        });
-        return;
-      }
-      res.status(500).json({ message: "Internal server error" });
+    const { groupData } = req.body;
+    const mainImage = req.file;
+    const data: GroupCreateRequest = JSON.parse(groupData);
+    const owner = userService.findUserById(data.ownerId);
+    if (!owner) {
+      throw new UserNotFoundError(`Owner with id ${data.ownerId} not found.`);
     }
-  };
+    const allowedImageTypes = ["image/jpeg", "image/png", "image/jpg"];
+    if (!allowedImageTypes.includes(mainImage.mimetype)) {
+      throw new ApiValidationError(
+        "Invalid image type. Only JPG, PNG, and JPEG are allowed.",
+        []
+      );
+    }
+
+    const userId = req.user.userId;
+    if (userId != data.ownerId) {
+      throw new ApiValidationError(
+        "Cannot use another owner as a group owner.",
+        []
+      );
+    }
+
+    const genreArray: Array<MusicGenre> = data.genres;
+    const newGroup = await groupService.createNewGroup(
+      data.ownerId,
+      data.name,
+      genreArray,
+      mainImage
+    );
+    res.status(201).json(newGroup);
+  } catch (error: unknown) {
+    logger.error("Create new group error: ", error);
+    if (error instanceof ApiError) {
+      res.status(error.statusCode).json({
+        message: error.message,
+        ...(error instanceof ApiValidationError && { errors: error.errors })
+      });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getGroup = async (
+  req: IRequestUser,
+  res: Response<GroupGetResponse | { message: string }>
+) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+
+    if (isNaN(id)) {
+      res
+        .status(HttpStatusCode.BAD_REQUEST)
+        .json({ message: "Invalid group ID." });
+      return;
+    }
+
+    const getGroupResponse = await groupService.getGroup(id);
+
+    res.status(HttpStatusCode.OK).json(getGroupResponse);
+  } catch (error: unknown) {
+    logger.error("Getting group error: ", error);
+    if (error instanceof ApiError) {
+      res.status(error.statusCode).json({
+        message: error.message,
+        ...(error instanceof ApiValidationError && { errors: error.errors })
+      });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getGroupsByUser = async (
+  req: IRequestUser,
+  res: Response<GroupGetByUserResponse | { message: string }>
+) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+
+    if (isNaN(id)) {
+      res
+        .status(HttpStatusCode.BAD_REQUEST)
+        .json({ message: "Invalid user ID." });
+      return;
+    }
+
+    const getGroupsByUserResponse = await groupService.getGroupsByUser(id);
+
+    res.status(HttpStatusCode.OK).json(getGroupsByUserResponse);
+  } catch (error: unknown) {
+    logger.error("Getting user groups error: ", error);
+    if (error instanceof ApiError) {
+      res.status(error.statusCode).json({
+        message: error.message,
+        ...(error instanceof ApiValidationError && { errors: error.errors })
+      });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getGroupRequests = async (
+  req: IRequestUser,
+  res: Response<GroupGetRequestsResponse | { message: string }>
+) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+
+    if (isNaN(id)) {
+      res
+        .status(HttpStatusCode.BAD_REQUEST)
+        .json({ message: "Invalid group ID." });
+      return;
+    }
+
+    const getGroupRequestsResponse = await groupService.getGroupRequests(id);
+
+    res.status(HttpStatusCode.OK).json(getGroupRequestsResponse);
+  } catch (error: unknown) {
+    logger.error("Getting group error: ", error);
+    if (error instanceof ApiError) {
+      res.status(error.statusCode).json({
+        message: error.message,
+        ...(error instanceof ApiValidationError && { errors: error.errors })
+      });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const searchGroups = async (req: Request, res: Response) => {
-    try {
-        // Validate query parameters
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            throw new ApiValidationError("Validation failed", errors.array());
-        }
-
-        const { name, genres, pageSize, pageNumber } = req.query;
-
-        // If genres is provided, parse it correctly into an array
-        let genresArray: string[] = [];
-        if (genres) {
-            try {
-                genresArray = JSON.parse(genres as string);
-            } catch (error: unknown) {
-                logger.error(error);
-                throw new ApiValidationError("Genres parameter must be a valid JSON array", []);
-            }
-        }
-
-        // Default pageSize and pageNumber if not provided
-        const pageSizeInt = pageSize ? parseInt(pageSize as string) : 20;
-        const pageNumberInt = pageNumber ? parseInt(pageNumber as string) : 1;
-
-        // Call the service to search groups
-        const groups = await groupService.searchGroup(
-            pageSizeInt.toString(),
-            pageNumberInt.toString(),
-            name as string | undefined,
-            genresArray as MusicGenre[]
-        );
-
-        res.status(200).json(groups);
-    } catch (error: unknown) {
-        logger.error("Search groups error: ", error);
-        if (error instanceof ApiError) {
-            res.status(error.statusCode).json({
-                message: error.message,
-                ...(error instanceof ApiValidationError && { errors: error.errors })
-            });
-            return;
-        }
-        res.status(500).json({ message: "Internal server error" });
+  try {
+    // Validate query parameters
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new ApiValidationError("Validation failed", errors.array());
     }
+
+    const { name, genres, pageSize, pageNumber } = req.query;
+
+    // If genres is provided, parse it correctly into an array
+    let genresArray: string[] = [];
+    if (genres) {
+      try {
+        genresArray = JSON.parse(genres as string);
+      } catch (error: unknown) {
+        logger.error(error);
+        throw new ApiValidationError(
+          "Genres parameter must be a valid JSON array",
+          []
+        );
+      }
+    }
+
+    // Default pageSize and pageNumber if not provided
+    const pageSizeInt = pageSize ? parseInt(pageSize as string) : 20;
+    const pageNumberInt = pageNumber ? parseInt(pageNumber as string) : 1;
+
+    // Call the service to search groups
+    const groups = await groupService.searchGroup(
+      pageSizeInt.toString(),
+      pageNumberInt.toString(),
+      name as string | undefined,
+      genresArray as MusicGenre[]
+    );
+
+    res.status(200).json(groups);
+  } catch (error: unknown) {
+    logger.error("Search groups error: ", error);
+    if (error instanceof ApiError) {
+      res.status(error.statusCode).json({
+        message: error.message,
+        ...(error instanceof ApiValidationError && { errors: error.errors })
+      });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const joinGroup = async (req: Request, res: Response) => {
+  try {
+    const group_id = parseInt(req.params.id, 10);
+    const user_id = req.body.userId;
+
+    if (isNaN(group_id)) {
+      res
+        .status(HttpStatusCode.BAD_REQUEST)
+        .json({ message: "Invalid group ID." });
+      return;
+    }
+
+    const joinGroupResponse = await groupService.joinGroup(group_id, user_id);
+
+    res.status(HttpStatusCode.OK).json(joinGroupResponse);
+  } catch (error: unknown) {
+    logger.error("Joining group error: ", error);
+    if (error instanceof ApiError) {
+      res.status(error.statusCode).json({
+        message: error.message,
+        ...(error instanceof ApiValidationError && { errors: error.errors })
+      });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateGroupRequest = async (req: IRequestUser, res: Response) => {
+  try {
+    const group_id = parseInt(req.params.id, 10);
+    const user_id = req.body.userId;
+    const action = req.body.action;
+    const owner_id = req.body.ownerId;
+
+    if (isNaN(group_id)) {
+      res
+        .status(HttpStatusCode.BAD_REQUEST)
+        .json({ message: "Invalid group ID." });
+      return;
+    }
+
+    if (action == "accept") {
+      const acceptJoinResponse = await groupService.acceptJoinRequest(
+        group_id,
+        user_id,
+        owner_id
+      );
+
+      res.status(HttpStatusCode.OK).json(acceptJoinResponse);
+    } else if (action == "deny") {
+      await groupService.denyJoinRequest(group_id, user_id, owner_id);
+      res.status(HttpStatusCode.OK).json("Request denied");
+    }
+  } catch (error: unknown) {
+    logger.error("Updating request error: ", error);
+    if (error instanceof ApiError) {
+      res.status(error.statusCode).json({
+        message: error.message,
+        ...(error instanceof ApiValidationError && { errors: error.errors })
+      });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
--- src/interface/group.ts
+++ src/interface/group.ts
@@ -1,29 +1,64 @@
 import { MusicGenre } from "@models/user";
+import { Group } from "@models/group";
+import { GroupRequest } from "@models/groupRequest";
 
 export interface GroupCreateRequest {
-    name: string;                
-    ownerId: number;             
-    mainImageUrl: string;        
-    genres: MusicGenre[];        
+  name: string;
+  ownerId: number;
+  mainImageUrl: string;
+  genres: MusicGenre[];
 }
 
-
 export interface GroupCreateResponse {
-    id: number;
-    name: string;
-    ownerId: number;
-    mainImageUrl: string;
-    genres: MusicGenre[];
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  name: string;
+  ownerId: number;
+  mainImageUrl: string;
+  genres: MusicGenre[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface GroupGetRequest {
-    id: number;
+  id: number;
 }
 
 export interface GroupGetResponse {
-    name: string;
-    mainImageUrl: string;
-    genres: MusicGenre[];
+  name: string;
+  mainImageUrl: string;
+  genres: MusicGenre[];
 }
+
+export interface GroupJoinRequest {
+  id: number;
+}
+
+export interface GroupJoinResponse {
+  user_id: number;
+  group_id: number;
+  sentAt: Date;
+  status: string;
+}
+
+export interface GroupGetByUserRequest {
+  id: number;
+}
+
+export interface GroupGetByUserResponse {
+  groups: Group[];
+}
+
+export interface GroupGetRequestsRequest {
+  id: number;
+}
+
+export interface GroupGetRequestsResponse {
+  requests: GroupRequest[];
+}
+
+export interface GroupUpdateRequestResponse {
+  group_id?: number;
+  user_id?: number;
+  role?: string;
+}
+
