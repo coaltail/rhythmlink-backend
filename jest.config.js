b0e0dbb4ad16cd95a3791918722d3eda9e4621c0
tcosic22 <tcosic22@student.foi.hr>
2025-02-13 18:05:15 +0100

    feat: Implement unit test for Login
    Added loginUser.test.ts and required modules in package.json and tsconfig.json.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  moduleNameMapper: {
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@routes/(.*)$": "<rootDir>/src/routes/$1",
+    "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
+    "^@controllers/(.*)$": "<rootDir>/src/controllers/$1",
+    "^@models/(.*)$": "<rootDir>/src/models/$1",
+    "^@services/(.*)$": "<rootDir>/src/services/$1",
+    "^@interface/(.*)$": "<rootDir>/src/interface/$1",
+    "^@common/(.*)$": "<rootDir>/src/common/$1"
+  },
+  transform: {
+    "^.+\\.ts?$": "ts-jest"
+  },
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
